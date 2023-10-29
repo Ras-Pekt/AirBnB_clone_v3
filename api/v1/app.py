03358b431a1f419ea33515b7d79b6b0586153aaa
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-10-29 19:00:10 +0300

    fix:pycodestyle

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,6 +12,7 @@ from api.v1.views import app_views
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def remove_session(err):
     """
@@ -21,12 +22,9 @@ def remove_session(err):
 
 
 if __name__ == "__main__":
-    """entry point
     """
-    host = getenv('HBNB_API_HOST')
-    port = getenv('HBNB_API_PORT')
-    if not host:
-        host = '0.0.0.0'
-    if not port:
-        port = '5000'
+    entry point
+    """
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = getenv('HBNB_API_PORT', 5000)
     app.run(host=host, port=port, threaded=True)
