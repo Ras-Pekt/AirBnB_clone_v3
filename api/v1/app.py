e13865b220842540137239fb02b2815eb1ddb1a9
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-10-29 14:29:23 +0300

    blueprints

---
+++ api/v1/app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+api version one
+"""
+
+from flask import Flask, Blueprint
+from os import getenv
+from models import storage
+from api.v1.views import app_views
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views, url_prefix="/api/v1")
+
+@app.teardown_appcontext
+def remove_session(e):
+    """
+    removes the current SQLAlchemy Session
+    """
+    storage.close()
+
+
+if __name__ == "__main__":
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = getenv("HBNB_API_PORT", 5000)
+
+    app.run(host=host, port=port, threaded=True)
