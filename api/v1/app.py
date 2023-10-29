df98b535b4430ca48a8b47305ccf94351bbb33fb
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-10-29 21:06:48 +0300

    error handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 api version one
 """
 
-from flask import Flask, Blueprint
+from flask import Flask, jsonify
 from os import getenv
 from models import storage
 from api.v1.views import app_views
@@ -19,6 +19,14 @@ def remove_session(err):
     removes the current SQLAlchemy Session
     """
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """
+    returns a JSON-formatted 404 status code response
+    """
+    return jsonify({'error': 'Not found'}), 404
 
 
 if __name__ == "__main__":
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,13 @@ index file
 """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route("/status", methods=["GET"], strict_slashes=False)
@@ -12,3 +19,20 @@ def status_route():
     returns status of route
     """
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats", methods=['GET'], strict_slashes=False)
+def count_status():
+    """
+    retrieves the number of each objects by type
+    """
+    count_dict = {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User),
+    }
+
+    return count_dict
